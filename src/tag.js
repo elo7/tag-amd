9807256939dff5a07fa20ce2d9acbdf2a802622d
Fernanda Moraes Bernardo and Luiz Corte Real <login+FernandaBernardo+luiz@elo7.com>
2016-09-22 16:22:36 -0300

    new test with enter is typed

--- src/tag.js
+++ src/tag.js
@@ -1,13 +1,15 @@
 define('tag', ['doc'], function($) {
 	'use strict';
 
-	var COMMA = 188;
+	var COMMA = 188,
+		ENTER = 13;
 
 	var Tag = function($element) {
 		var tags = [];
 
 		$element.on('keydown', function(e) {
-			if (e.which === COMMA || e.keyCode === COMMA) {
+			if (e.which === COMMA || e.keyCode === COMMA ||
+				e.which === ENTER || e.keyCode === ENTER) {
 				e.preventDefault();
 				addTags();
 			}

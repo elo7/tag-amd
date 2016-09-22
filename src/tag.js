f2a15bc6fbc6086fa9fa0874b65b57476a962f43
Fernanda Moraes Bernardo and Luiz Corte Real <login+FernandaBernardo+luiz@elo7.com>
2016-09-22 16:19:39 -0300

    improve tests code and new test with interactions

--- src/tag.js
+++ src/tag.js
@@ -1,17 +1,34 @@
 define('tag', ['doc'], function($) {
 	'use strict';
 
-	var Tag = function(element) {
-		this.tags = function() {
-			return element.val().split(/\s*,\s*/).filter(function(tag) {
+	var COMMA = 188;
+
+	var Tag = function($element) {
+		var tags = [];
+
+		$element.on('keydown', function(e) {
+			if (e.which === COMMA || e.keyCode === COMMA) {
+				e.preventDefault();
+				addTags();
+			}
+		});
+
+		var addTags = function() {
+			var tagsToAdd = $element.val().split(/\s*,\s*/).filter(function(tag) {
 				return tag.length > 0;
 			});
+			tags = tags.concat(tagsToAdd);
+		};
+
+		addTags();
+		this.tags = function() {
+			return tags;
 		};
 	};
 
 	var tagify = function(selector) {
-		var element = $(selector);
-		return new Tag(element)
+		var $element = $(selector);
+		return new Tag($element);
 	};
 
 	return {

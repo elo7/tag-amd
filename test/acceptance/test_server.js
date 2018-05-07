2cb64a40fd470d8fe4135eb21ef526b612e1f556
Leonardo Souza <login+leonardosouza@elo7.com>
2018-05-07 17:16:20 -0300

    changing strategy to run server

--- test/acceptance/test_server.js
+++ test/acceptance/test_server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.static(__dirname));
 app.use(express.static(__dirname + '/../../src'));
 app.use(express.static(__dirname + '/../../bower_components'));
 app.use(express.static(__dirname + '/../../node_modules'));
 
-app.listen(4000, function() {
-	console.log('Server started!');
+app.listen(PORT, function() {
+	console.log(`Server running in port http://localhost:${PORT}`);
 });
--- test_dev.sh
+++ test_dev.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-lsof -Pi | grep ':4000' | awk '{print $2}' | xargs kill -9
-./parallel_commands.sh "npm run test:server" "npm run cypress:open"
+random_port=$(( $RANDOM % 65536 ));
+
+counter=0
+while [ $counter -lt 1 ]
+do
+	if [ -n "$(lsof -i :$random_port)" ]
+	then
+		echo "port in use!";
+	else
+		echo "{\"baseUrl\":\"http://localhost:${random_port}\",\"watchForFileChanges\":true,\"videoRecording\":false}" > cypress.json
+		PORT=$random_port node test/acceptance/test_server.js & pid=$!
+		PID_LIST+=" $pid";
+		npm run cypress:open & pid=$!
+		PID_LIST+=" $pid";
+
+		trap "kill $PID_LIST" SIGINT
+		echo "Parallel processes have started";
+		wait $PID_LIST
+		echo "All processes have completed";
+		((counter++))
+	fi
+done
